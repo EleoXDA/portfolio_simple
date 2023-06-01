6aa34e68c37d24d5f44e99f75c1ce2ba0beddbdd
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-06-01 19:58:54 +0200

    update theme and add theme toggle

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function showContent(contentId) {
       }
   }
 }
+
+document.getElementById('theme-toggle').addEventListener('click', function() {
+  let root = document.documentElement;
+  let bgColor = getComputedStyle(root).getPropertyValue('--color-background-light');
+  if (bgColor.trim() == '#F2F2F2') {
+    root.style.setProperty('--color-background-light', '#000000');
+    root.style.setProperty('--color-text-light', '#F2F2F2');
+    root.style.setProperty('--color-button-light', '#a5a5a5');
+    root.style.setProperty('--color-button-text-light', '#F2F2F2');
+
+  } else {
+    root.style.setProperty('--color-background-light', '#F2F2F2');
+    root.style.setProperty('--color-text-light', '#000000');
+    root.style.setProperty('--color-button-light', '#999999');
+    root.style.setProperty('--color-button-text-light', '#000000');
+  }
+});
