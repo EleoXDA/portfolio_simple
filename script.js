3efef3e3244204c59b8a07ac731a5b9bff286d4c
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-06-01 19:40:05 +0200

    add more styling and functions to the page

---
+++ script.js
@@ -0,0 +1,10 @@
+function showContent(contentId) {
+  var contentElements = document.getElementsByClassName("content");
+  for(var i = 0; i < contentElements.length; i++) {
+      if(contentElements[i].id === contentId) {
+          contentElements[i].style.display = "block";
+      } else {
+          contentElements[i].style.display = "none";
+      }
+  }
+}
